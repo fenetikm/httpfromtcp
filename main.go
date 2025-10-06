d12c61e4b307d98a0fd2f0226f34e8999b2a9dec
Michael Welford <mail@michaelwelford.com>
2025-10-06 22:02:56 +1030

    Convert line reading to use channels.

--- main.go
+++ main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+	lines := make(chan string)
+	go func() {
+		currentline := ""
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			parts := strings.Split(string(b), "\n")
+			currentline += parts[0]
+			if len(parts) == 1 {
+			} else {
+				currentline = strings.Replace(currentline, "\n", "", 1)
+				lines <- currentline
+				// fmt.Printf("read: %s\n", currentline)
+				currentline = parts[1]
+			}
+			if err != nil || n == 0 {
+				break
+			}
+		}
+		close(lines)
+	}()
+
+	return lines
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
@@ -14,20 +42,7 @@ func main() {
 	}
 	defer file.Close()
 
-	currentline := ""
-	for {
-		b := make([]byte, 8)
-		n, err := file.Read(b)
-		parts := strings.Split(string(b), "\n")
-		currentline += parts[0]
-		if len(parts) == 1 {
-		} else {
-			currentline = strings.Replace(currentline, "\n", "", 1)
-			fmt.Printf("read: %s\n", currentline)
-			currentline = parts[1]
-		}
-		if err != nil || n == 0 {
-			break
-		}
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
 }

f67473b71de13e43bc1c8e977811a67695f06c77
Michael Welford <mail@michaelwelford.com>
2025-10-06 21:36:35 +1030

    Get line reader v0 working.

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatalf("Error opening file.")
+	}
+	defer file.Close()
+
+	currentline := ""
+	for {
+		b := make([]byte, 8)
+		n, err := file.Read(b)
+		parts := strings.Split(string(b), "\n")
+		currentline += parts[0]
+		if len(parts) == 1 {
+		} else {
+			currentline = strings.Replace(currentline, "\n", "", 1)
+			fmt.Printf("read: %s\n", currentline)
+			currentline = parts[1]
+		}
+		if err != nil || n == 0 {
+			break
+		}
+	}
 }
